868dd0bfeadb7ca3b507657dbeaea7accd4ea37e
MattJCruz <MattJCruz@users.noreply.github.com>
2023-11-08 15:52:32 -0600

    Officially Starting Javascript
    Getting into the meat and potatoes of webdev, this is where I'll be making functional javascript components for my webpages.

--- JavascriptPractice.js
+++ JavascriptPractice.js
@@ -1,3 +1,41 @@
-var cheese = new Array();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var cheese = new Array();
 cheese[0] = "tom";
 console.log(cheese[0]);
+
+var arrayOne = new Array();
+            cheese[0] = "tom";
+            function init(){
+                var textbox1 = document.getElementById("test");
+                textbox1.innerHTML = "New text Added!";
+                var i = 0;
+                while(i<10)
+                    {
+                        arrayOne[i]=i;
+                        console.log(arrayOne[0]);
+                        i++;
+                    }
+            }
+            window.onload = init;
+            */
