6581746830e2d8e0096e90749603604691d2072e
MattJCruz <MattJCruz@users.noreply.github.com>
2024-01-24 23:31:25 -0600

    Java Addition
    Adding some Java learning material to the project

--- TestJava.java
+++ TestJava.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
+
+int attribute;
+//String will hold a string of attribute place values.
+//36 digits will provide enough diversity to define certain 
 public class TestJava
 {
+    //String of attribute characters
+    String name;
+    
+    //List (att)ributes here
+    char att0;
+    char att1;
+    char att2;
+    char att3;
+    char att4;
+    char att5;
+    char att6;
+    
+    //Method for assigning attributes to String
     public static void main(String[]args)
     {
+        //Traverse the String character by character and assign attribute at each position. Base this on 
+        Scanner kb = new Scanner(System.in);
+        //Temporary method of intake for attributes, to be done by checkbox later.
         
     }
+    
+    //Method for extracting current file name, storing it in text file alongside new name, and maybe 
+    
+    public void Assigner()
+    {
+            
+    }
+    if (charAt(23))
 }
